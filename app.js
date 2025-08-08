6554e2f16872c662ba5197c5d1410f990fc4bc3e
Jinshad-git <jinshadjinu158@gmail.com>
2025-08-08 19:42:17 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExcuseGenerator from './ExcuseGenerator'; // Adjust the path as needed
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ExcuseGenerator />
+    </div>
+  );
+}
+
+export default App;
